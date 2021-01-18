3a74cfbc1883d0c4bad46665d937f83860e694bc
MateuszLason <crew1997@gmail.com>
2021-01-18 02:17:00 +0100

    html, css and working script

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,22 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const buttonAdd = document.querySelector('.button__dodaj--js');
+const buttonSubtract = document.querySelector('.button__usun--js');
+let number = 0;
+document.querySelector('.liczba--js').innerHTML = number;
+
+buttonAdd.addEventListener('click', () => {
+number = number + 1;
+document.querySelector('.liczba--js').innerHTML = number;
+});
+
+buttonSubtract.addEventListener('click', () => {
+number = number - 1;
+document.querySelector('.liczba--js').innerHTML = number;
+if (number < 0) {
+number = 0;
+document.querySelector('.liczba--js').innerHTML = number;
+}
+});
